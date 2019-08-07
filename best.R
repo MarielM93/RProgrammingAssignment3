be8d3c6b25a345a097ccd16c96f0b9c47e53cff9
MarielM93 <mariel.maniclang@gmail.com>
2019-08-07 18:00:11 +0800

    initial commit

---
+++ best.R
@@ -0,0 +1,48 @@
+## "best" finds the name of the hospital that has the lowest 30-day mortality
+## for the specified outcome in that state
+## this R script will be making use of the dplyr package
+library(dplyr)
+
+best <- function(state, outcome){
+        ## arguments:
+        ## state = abbreviated name of a state to query from
+        ## outcome = a specified condition to query outcomes from
+        ## note:  [,11] = heart attack
+        ##        [,17] = heart failure
+        ##        [,23] = pneumonia
+  
+        ## read outcome data
+        caremeasures <- read.csv("outcome-of-care-measures.csv",
+                                  header = TRUE,
+                                  na.strings = "Not Available",
+                                  stringsAsFactors = FALSE)
+        caremeasures <- tbl_df(caremeasures)
+  
+        ## check that outcome are valid
+        validoutcomes <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+        if(outcome %in% names(validoutcomes)){
+                outcomecol <- validoutcomes[outcome]
+        }
+        else{
+                print("invalid outcome")
+                stop()
+        }
+        
+        ## check that state is valid
+        if(state %in% caremeasures$State == FALSE){
+                print("invalid state")
+                stop()
+        }
+        
+        ## subset and rank relevant parameters
+        queryfields <- caremeasures %>%
+                      select(2, 7, outcomecol) %>%
+                      filter(State == state)
+        
+        colnames(queryfields) <- c("Name", "State", "Rate")
+        is.numeric(queryfields$Rate)
+        ranked <- arrange(queryfields, Rate, Name)
+        
+        print(as.character(ranked[1,1]))
+
+}
